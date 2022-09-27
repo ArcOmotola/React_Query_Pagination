1b3c0ad04cb3473e7cfaa2cd86dbadb7a396fd8f
Omotola Ogunsola <ogunsola40@gmail.com>
2022-09-27 23:16:45 +0100

    edited db

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -2,68 +2,64 @@ import { useQuery, useMutation, useQueryClient } from "react-query";
 import { request } from "../utils/axios-utils";
 
 const fetchSuperHeroes = () => {
-    // return axios.get("http://localhost:4000/superheroes");
-    return request({ url: "/superheroes"});
-}
+  // return axios.get("http://localhost:4000/superheroes");
+  return request({ url: "/superheroes" });
+};
 
 const addSuperHero = (hero) => {
-    // return axios.post("http://localhost:4000/superheroes", hero)
-    return request({ url: "/superheroes", method: "post", data: hero})
-}
+  // return axios.post("http://localhost:4000/superheroes", hero)
+  return request({ url: "/superheroes", method: "post", data: hero });
+};
 
 export const useSuperHeroesData = (onSuccess, onError) => {
-    return useQuery("super-heroes", fetchSuperHeroes,
-        {
-            onSuccess,
-            onError,
-            // select: (data) => {
-            //     const superHeroNames = data.data.map((hero) => hero.name)
-            //     return superHeroNames;
-            // }
-        });
-}
+  return useQuery("super-heroes", fetchSuperHeroes, {
+    onSuccess,
+    onError,
+    // select: (data) => {
+    //     const superHeroNames = data.data.map((hero) => hero.name)
+    //     return superHeroNames;
+    // }
+  });
+};
 
 export const useAddSuperHeroData = () => {
-    const queryClient = useQueryClient();
-    return useMutation(addSuperHero, {
-        // onSuccess: (data) => {
-        //     // queryClient.invalidateQueries("super-heroes")        //refteches query once rendered as invalid, after posting method is successful
-            
-        //     queryClient.setQueryData("super-heroes", (oldQueryData) => {
-        //         return {
-        //             ...oldQueryData,
-        //             data: [...oldQueryData.data, data.data],       //updates list from local response without trigerring network call 
-        //         }
-        //     })
-        // }
+  const queryClient = useQueryClient();
+  return useMutation(addSuperHero, {
+    // onSuccess: (data) => {
+    //     // queryClient.invalidateQueries("super-heroes")        //refteches query once rendered as invalid, after posting method is successful
 
-        onMutate: async (newHero) => {
-            await queryClient.cancelQueries("super-heroes")
-            const previousHeroData = queryClient.getQueryData("super-heroes")
-            queryClient.setQueryData("super-heroes", (oldQueryData) => {
-                return {
-                    ...oldQueryData,
-                    data: [
-                        ...oldQueryData.data,
-                        { id: oldQueryData?.data?.length + 1, ...newHero },
-                    ],
-                }
-            })
+    //     queryClient.setQueryData("super-heroes", (oldQueryData) => {
+    //         return {
+    //             ...oldQueryData,
+    //             data: [...oldQueryData.data, data.data],       //updates list from local response without trigerring network call
+    //         }
+    //     })
+    // }
 
-            return {
-                previousHeroData,   //to roll back data in case of an error
-            }
-        },
-        onError: (_error, _hero, context) => {
-            queryClient.setQueryData("super-heros", context.previousHeroData)
-        },
-        onSettled: () => {
-            queryClient.invalidateQueries("super-heroes")
-        },
-    })
-}
+    onMutate: async (newHero) => {
+      await queryClient.cancelQueries("super-heroes");
+      const previousHeroData = queryClient.getQueryData("super-heroes");
+      queryClient.setQueryData("super-heroes", (oldQueryData) => {
+        return {
+          ...oldQueryData,
+          data: [
+            ...oldQueryData.data,
+            { id: oldQueryData?.data?.length + 1, ...newHero },
+          ],
+        };
+      });
 
-
-
+      return {
+        previousHeroData, //to roll back data in case of an error
+      };
+    },
+    onError: (_error, _hero, context) => {
+      queryClient.setQueryData("super-heros", context.previousHeroData);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries("super-heroes");
+    },
+  });
+};
 
 //sample queries
